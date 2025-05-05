a71cf8964b757138e10b3ecabe828abbfbbaa709
Kumar Anshuman <kanshuman6484@gmail.com>
2025-05-05 15:21:11 +0530

    created react app with vite and installed tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold p-2 text-center text-indigo-50 bg-green-900">
+        Welcome to Event Orbit!
+      </h1>
+    </>
+  )
+}
+
+export default App
